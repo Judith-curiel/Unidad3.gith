b30160aa77c44c40c48f6a088ffea892f90720f8
Judith-curiel <judithmontelongocuriel10@gmail.com>
2024-10-23 18:47:28 -0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+  function myFunction() {
+                document.getElementById('demo3').style.fontSize = '40px';
+                document.getElementById("demo3").innerHTML = "Cambió con JS Externo a color rosa y a 40px";
+                document.getElementById("demo3").style.color = "pink";
+            }
+            function aumentar() {
+                document.getElementById('demo2').style.fontSize = '50px';
+                document.getElementById('demo2').style.color = 'purple';
+            }
